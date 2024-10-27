7dee5f2a0c63d4f49511de13e862c891dc207860
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-10-28 02:05:23 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-[#0B2434] min-h-screen">
+      <main className="w-10/12 h-96 mx-auto border-2 border-green-50 rounded-md bg-[#F5F5F5]">
+        Dog
+      </main>
+    </div>
+  );
+}
+
+export default App;
